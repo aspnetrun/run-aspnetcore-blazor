02c594e8e0dbe96c63ed55f431d10b100f91e1ea
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-06 20:44:28 +0300

    IssueComment created

---
+++ src/AspnetRun.Core/Entities/IssueComment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Entities
+{
+    public class IssueComment : BaseEntity
+    {
+        public string Message { get; set; }
+        public int CreatorUserId { get; set; }
+        public IssueComment(int creatorUserId, string message)
+        {
+            Check.NotNull(creatorUserId, nameof(creatorUserId));
+            Check.NotNull(message, nameof(message));
+
+            Message = message;
+            CreatorUserId = creatorUserId;
+        }
+
+        public override string ToString()
+        {
+            return $"[IssueComment {Id}] {Message}";            
+        }
+    }
+}
