53aaaac5599e495f69e06b10fc798ecb79d8c165
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-08 16:06:35 +0300

    IssueAssignmentPolicy

--- src/AspnetRun.Core/Policy/IIssueAssignmentConfiguration.cs
+++ src/AspnetRun.Core/Policy/IIssueAssignmentConfiguration.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace AspnetRun.Core.Policy
 {
+    // TODO : check tennisbookings - this will be loaded from aspnet core DI - from appsettings.json
     public interface IIssueAssignmentConfiguration
     {
         int MaxConcurrentOpenIssueCountForAUser { get; set; }
---
+++ src/AspnetRun.Core/Policy/IssueAssignmentPolicy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspnetRun.Core.Entities;
+
+namespace AspnetRun.Core.Policy
+{
+    public class IssueAssignmentPolicy : IIssueAssignmentPolicy
+    {
+        public void CheckAssignment(Issue issue, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
