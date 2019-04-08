9aa11349ac58997317a878ec69f1ea847aedb77f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-08 16:03:26 +0300

    IIssueAssignmentConfiguration added

---
+++ src/AspnetRun.Core/Policy/IIssueAssignmentConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Policy
+{
+    public interface IIssueAssignmentConfiguration
+    {
+        int MaxConcurrentOpenIssueCountForAUser { get; set; }
+    }
+}
