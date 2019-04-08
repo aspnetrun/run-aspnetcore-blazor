6f2eed7a040930ea96df3d99cf46b7f39da36ff6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-08 16:09:06 +0300

    IIssueRepository

---
+++ src/AspnetRun.Core/Interfaces/IIssueRepository.cs
@@ -0,0 +1,13 @@
+﻿using AspnetRun.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Interfaces
+{
+    public interface IIssueRepository : IAsyncRepository<Issue>
+    {
+        int GetOpenIssueCountOfUser(string userId);
+        List<Issue> GetCommentsWithCreatorUsers(int issueId);
+    }
+}
--- src/AspnetRun.Core/Policy/IssueAssignmentPolicy.cs
+++ src/AspnetRun.Core/Policy/IssueAssignmentPolicy.cs
@@ -7,6 +7,15 @@ namespace AspnetRun.Core.Policy
 {
     public class IssueAssignmentPolicy : IIssueAssignmentPolicy
     {
+        private readonly IIssueRepository _issueRepository;
+        private readonly IIssueAssignmentConfiguration _configuration;
+
+        public IssueAssignmentPolicy(IIssueRepository issueRepository, IIssueAssignmentConfiguration configuration)
+        {
+            _issueRepository = issueRepository ?? throw new ArgumentNullException(nameof(issueRepository));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
         public void CheckAssignment(Issue issue, User user)
         {
             throw new NotImplementedException();
