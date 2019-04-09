707507d79833a6284f09ab7548e564edc7520b25
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-09 16:00:24 +0300

    IssueRepository

---
+++ src/AspnetRun.Infrastructure/Repository/IssueRepository.cs
@@ -0,0 +1,26 @@
+﻿using AspnetRun.Core.Entities;
+using AspnetRun.Core.Interfaces;
+using AspnetRun.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Infrastructure.Repository
+{
+    public class IssueRepository : AspnetRunRepository<Issue>, IIssueRepository
+    {
+        public IssueRepository(AspnetRunContext dbContext) : base(dbContext)
+        {
+        }
+
+        public List<Issue> GetCommentsWithCreatorUsers(int issueId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetOpenIssueCountOfUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
