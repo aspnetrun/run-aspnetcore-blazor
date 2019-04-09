298bbfd5d125ddaca9100964fd4802b8265af130
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-09 16:13:55 +0300

    IIssueAppService

---
+++ src/AspnetRun.Application/Interfaces/IIssueAppService.cs
@@ -0,0 +1,9 @@
+﻿namespace AspnetRun.Application.Interfaces
+{
+    public interface IIssueAppService
+    {
+        void AssignIssueToUser(AssignIssueToUserInput input);
+        void AddComment(AddCommentInput input);
+        GetIssueOutput GetIssue(GetIssueInput input);
+    }
+}
