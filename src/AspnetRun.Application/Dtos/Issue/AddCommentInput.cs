299f2426991cfa65e5f82bc81082d21284796b45
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-11 15:20:40 +0300

    AddCommentInput

---
+++ src/AspnetRun.Application/Dtos/Issue/AddCommentInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AspnetRun.Application.Dtos.Issue
+{
+    public class AddCommentInput
+    {
+        [Required]
+        [MaxLength(128)]
+        public int IssueId { get; set; }
+
+        [Required]
+        [MaxLength(65536)]
+        public string Message { get; protected set; }
+    }
+}
