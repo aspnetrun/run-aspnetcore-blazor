6c58a3a038e3e421e2d63abcd28c3178fe0364c2
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-11 15:06:40 +0300

    IssueCommentDto

---
+++ src/AspnetRun.Application/Dtos/IssueCommentDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Application.Dtos
+{
+    public class IssueCommentDto : BaseDto
+    {
+        public string Message { get; set; }
+        public UserDto CreatorUser { get; set; }
+    }
+}
