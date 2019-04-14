cd8125456dcea4928343b254de0553729b2c2acd
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-14 09:51:39 +0300

    ISessionService

---
+++ src/AspnetRun.Application/Session/ISessionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Application.Session
+{
+    public interface ISessionService
+    {
+        string UserId { get; set; }
+    }
+}
