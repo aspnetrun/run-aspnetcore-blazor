17b458fa58e077134adbff947d840fb188169f50
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-14 09:50:21 +0300

    IValidationService

---
+++ src/AspnetRun.Application/Validation/IValidationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Application.Validation
+{
+    public interface IValidationService
+    {
+        void Validate(object obj);
+    }
+}
