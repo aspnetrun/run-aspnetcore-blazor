75b83b67210c4f8ba2a65f5bfb179fae58e2b383
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-14 09:52:19 +0300

    NullSessionService

---
+++ src/AspnetRun.Application/Session/NullSessionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Application.Session
+{
+    public class NullSessionService : ISessionService
+    {
+        public static NullSessionService Instance { get; } = new NullSessionService();
+        public string UserId { get; set; }
+
+        private NullSessionService()
+        {
+        }
+    }
+}
